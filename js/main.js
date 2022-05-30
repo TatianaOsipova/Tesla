6e6aa13d4fabb70c52c2b41689be57f74087ac0d
Tatiana Osipova <otv120780@gmail.com>
2022-05-30 08:43:23 -0400

    Feat: added main js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,7 @@
-$(function(){
-    $('.fairy-tail__slider').slick({
-        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
-        autoplay: true,
-        fade: true,
-        responsive: [ 
-            {   breakpoint: 601, 
-                settings: { 
-                arrows: false }
-            }, 
-        ]
+var SweetModal = require('./components/SweetModal.vue')
+var SweetModalTab = require('./components/SweetModalTab.vue')
 
-    });
-
-    $('.our-trip__slider').slick({
-        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
-        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
-        autoplay: true,
-        fade: true,
-        responsive: [ 
-            {   breakpoint: 601, 
-                settings: { 
-                arrows: false }
-            }, 
-        ]
-    });
-});
-
-
-
-
+module.exports = {
+	SweetModal: SweetModal,
+	SweetModalTab: SweetModalTab
+}
