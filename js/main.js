bc5434879076ba8e815b29b1382d25b9ab3ed760
Tatiana Osipova <otv120780@gmail.com>
2022-05-30 09:11:50 -0400

    Feat: added slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-var SweetModal = require('./components/SweetModal.vue')
-var SweetModalTab = require('./components/SweetModalTab.vue')
-
-module.exports = {
-	SweetModal: SweetModal,
-	SweetModalTab: SweetModalTab
-}
-
 $(function(){
-$('.slider')
-	
+  $('.slider').slick({
+	  
 
+  });
 });
